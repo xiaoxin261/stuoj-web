dc1e29a5e457a1b5376d396df048adcb62280184
ArtdragonXoX <1262922587@qq.com>
2024-12-24 01:58:47 +0800

    feat(components): 新增测试点编辑组件
    - 在 components.d.ts 中添加 TestcaseEdit 和 TestCaseEdit 组件的声明
    - 实现 TestCaseEdit 组件，用于编辑测试点信息
    - 组件包含测试点 ID、题目 ID、测试点编号、输入、输出等字段
    - 提供提交和重置功能

---
+++ src/components/problem/TestCaseEdit.vue
@@ -0,0 +1,57 @@
+<template>
+    <ElForm :model="localTestcase" label-width="auto">
+        <ElFormItem label="测试点ID" label-position="right">
+            <ElInput v-model="localTestcase.id" disabled />
+        </ElFormItem>
+        <ElFormItem label="题目ID" label-position="right">
+            <ElInput v-model="localTestcase.problem_id" disabled />
+        </ElFormItem>
+        <ElFormItem label="测试点编号" label-position="right">
+            <ElInput v-model="localTestcase.serial" disabled />
+        </ElFormItem>
+        <ElFormItem label="输入" label-position="right">
+            <ElInput v-model="localTestcase.test_input" type="textarea" />
+        </ElFormItem>
+        <ElFormItem label="输出" label-position="right">
+            <ElInput v-model="localTestcase.test_output" type="textarea" />
+        </ElFormItem>
+        <ElFormItem>
+            <ElButton type="primary" @click="handleSubmit">提交</ElButton>
+            <ElButton @click="handleReset">重置</ElButton>
+        </ElFormItem>
+    </ElForm>
+</template>
+
+<script setup lang="ts">
+import type { Testcase } from '@/types/Problem';
+import { ref, watch } from 'vue';
+
+const props = withDefaults(defineProps<{
+    testcase?: Testcase,
+}>(),{
+    testcase: () => ({
+        id: 0,
+        serial: 0,
+        problem_id: 0,
+        test_input: '',
+        test_output: '',
+    }),
+});
+const emit = defineEmits(['update:testcase']);
+
+const localTestcase = ref<Testcase>({ ...props.testcase });
+
+const handleSubmit = async () => {
+    emit('update:testcase', localTestcase.value);
+};
+
+const handleReset = () => {
+    if (props.testcase)
+        localTestcase.value = { ...props.testcase };
+};
+
+watch(props, () => {
+    if (props.testcase)
+        localTestcase.value = { ...props.testcase };
+});
+</script>
