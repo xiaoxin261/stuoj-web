b1e39a4bbed73baa7ab476725ddb7edc0f7ef2e0
22zwxia@stu.edu.cn <22zwxia@stu.edu.cn>
2024-12-23 12:59:36 +0800

    feat(auth): 优化 AI 题目生成页面
    - 重新设计了页面布局和样式，增加了更多视觉元素
    - 添加了加载状态和相应的动画效果
    - 优化了表单输入的样式，使其更加美观
    - 更新了按钮样式，增加了开始生成题目的动效

--- src/views/auth/AIProcess.vue
+++ src/views/auth/AIProcess.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="algorithm-form">
     <div class="form-input">
-      <label for="title">标题:</label>
-      <el-input
+      <label for="title"></label>
+      <input class="input" 
         id="title"
         v-model="req.title"
         placeholder="请输入标题..."
-      ></el-input>
+      ></input>
     </div>
     <div class="form-input">
-      <label for="description">算法情景描述:</label>
-      <textarea
-        id="description"
+      <label for="description"></label>
+      <textarea class="input"         id="description"
         v-model="req.description"
         placeholder="请输入算法情景描述..."
         rows="15"
-        cols="50"
-      ></textarea>
+        cols="50"></textarea>
+      <!-- <textarea
+
+      ></textarea> -->
       <div class="form-tags">
         <label>算法标签:</label>
         <el-form-item label-width="auto">
@@ -24,11 +25,69 @@
         </el-form-item>
       </div>
     </div>
-      <el-button type="primary" @click="submitForm">确定</el-button>
+    <button class="uiverse"  @click="submitForm">
+  <div class="wrapper">
+    <span>开始生成题目</span>
+    <div class="circle circle-12"></div>
+    <div class="circle circle-11"></div>
+    <div class="circle circle-10"></div>
+    <div class="circle circle-9"></div>
+    <div class="circle circle-8"></div>
+    <div class="circle circle-7"></div>
+    <div class="circle circle-6"></div>
+    <div class="circle circle-5"></div>
+    <div class="circle circle-4"></div>
+    <div class="circle circle-3"></div>
+    <div class="circle circle-2"></div>
+    <div class="circle circle-1"></div>
+  </div>
+</button>
+      <!-- <el-button type="primary" @click="submitForm">确定</el-button> -->
     </div>
+    <div v-if="isLoading" class="loader-container">
+      <div   class="loader" >
+  <div >
+    <ul>
+      <li>
+        <svg fill="currentColor" viewBox="0 0 90 120">
+          <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+        </svg>
+      </li>
+      <li>
+        <svg fill="currentColor" viewBox="0 0 90 120">
+          <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+        </svg>
+      </li>
+      <li>
+        <svg fill="currentColor" viewBox="0 0 90 120">
+          <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+        </svg>
+      </li>
+      <li>
+        <svg fill="currentColor" viewBox="0 0 90 120">
+          <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+        </svg>
+      </li>
+      <li>
+        <svg fill="currentColor" viewBox="0 0 90 120">
+          <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+        </svg>
+      </li>
+      <li>
+        <svg fill="currentColor" viewBox="0 0 90 120">
+          <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z"></path>
+        </svg>
+      </li>
+    </ul>
+  </div><span style="color: white;">AI生成中...</span></div>
+    </div>
+
   </template>
   
   <script setup lang="ts">
+  import '@/assets/aiprocess/proceedButton.css';
+  import '@/assets/aiprocess/textInput.css'
+    import '@/assets/aiprocess/loadingConponent.css'
   import { ref ,onMounted} from 'vue';
   import { ElCheckbox, ElCheckboxGroup, ElButton } from 'element-plus';
   import router from '@/router';
@@ -40,14 +99,19 @@
   import { userStore } from '@/stores/user';
   import ProblemTagSelect from '@/components/problem/ProblemTagSelect.vue';
 
-  // 响应式数据
   const req = ref<ProblemSubmit>({title:'',description:'',tags:[]});
   const aiProblemInfo = ref<ProblemInfo | null>(null);
   const {execute:submitInfo, state:curProblemInfo} = aiProcessApi();
   const {execute:submitProblem, state:curProblemid} = uploadProblemApi();
   const { token } = userStore();
+  // 响应式变量，控制加载状态
+const isLoading = ref(false);
+
   // 提交表单的方法
   const submitForm = async () => {
+      // 开始加载
+  isLoading.value = true;
+
   // 控制台输出用户填写的文本区域和选择的算法标签
   console.log('标题:', req.value.title);
   console.log('算法情景描述:', req.value.description);
@@ -72,7 +136,9 @@ const output =aiProblemInfo.value?.output;
 const sample_input = aiProblemInfo.value?.sample_input;
 const sample_output = aiProblemInfo?.value?.sample_output;
 const hint = aiProblemInfo?.value?.hint;
-
+if (!aiProblemInfo.value) {
+  isLoading.value = false;
+}
   // 将 aiProblemInfo 作为路由参数传递
   router.push({
     path: '/problemdisplay',
