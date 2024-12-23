587fce76190e8792b9dd7d2a414ff109cde0dc0a
ArtdragonXoX <1262922587@qq.com>
2024-12-24 01:58:35 +0800

    feat(TestTable): 优化测试数据表格功能
    - 重构组件逻辑，提高代码可维护性
    - 添加测试数据更新和删除功能
    - 优化测试数据添加流程
    - 调整表格列宽，提升可读性

--- src/components/problem/TestTable.vue
+++ src/components/problem/TestTable.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="test-case-table">
         <div class="button-group">
-            <ElButton type="info" @click="refreshTestCases" icon="Refresh" style="width: 80px;">刷新</ElButton>
-            <ElButton type="primary" @click="addTestCase" icon="CirclePlus">添加测试数据</ElButton>
+            <ElButton type="info" @click="refreshTestcases" icon="Refresh" style="width: 80px;">刷新</ElButton>
+            <ElButton type="primary" @click="addTestcase" icon="CirclePlus">添加测试数据</ElButton>
         </div>
-        <ElTable :data="testCases" style="width: 100%;" @current-change="handleCurrentChange" highlight-current-row
+        <ElTable :data="testcases" style="width: 100%;" @current-change="handleCurrentChange" highlight-current-row
             stripe>
-            <ElTableColumn label="ID" prop="data.id" width="40" />
+            <ElTableColumn label="ID" prop="data.id" width="60" />
             <ElTableColumn label="编号" prop="data.serial" />
             <ElTableColumn label="脏位" prop="checked" width="65">
                 <template #default="scope">
@@ -28,51 +28,52 @@
                 <ElCheckbox v-model="scope.row.deleted" size="large" />
             </ElTableColumn>
         </ElTable>
-        <ElButton type="primary" @click="uploadTestCase" :icon="Upload" style="width: 100px; margin-top: 10px;">更新
+        <ElButton type="primary" @click="uploadTestcase" :icon="Upload" style="width: 100px; margin-top: 10px;">更新
         </ElButton>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { getTestCaseApi, uploadTestCaseApi, updateTestCaseApi, deleteTestCaseApi, getProblemApi } from '@/apis/problem';
+import { ref, watch } from 'vue';
+import { getTestcaseApi, uploadTestcaseApi, updateTestcaseApi, deleteTestcaseApi, getProblemApi } from '@/apis/problem';
 import type { Testcase } from '@/types/Problem';
 import { userStore } from '@/stores/user';
 import { ElMessage, ElTableColumn, ElTag } from 'element-plus';
 import { CircleCloseFilled, Warning, CircleCheck, Upload } from '@element-plus/icons-vue';
 
 const { execute: getProblemExecute } = getProblemApi();
-const { execute: getTestCaseExecute } = getTestCaseApi();
-const { execute: uploadTestCaseExecute } = uploadTestCaseApi();
-const { execute: updateTestCaseExecute } = updateTestCaseApi();
-const { execute: deleteTestCaseExecute } = deleteTestCaseApi();
+const { execute: getTestcaseExecute } = getTestcaseApi();
+const { execute: uploadTestcaseExecute } = uploadTestcaseApi();
+const { execute: updateTestcaseExecute } = updateTestcaseApi();
+const { execute: deleteTestcaseExecute } = deleteTestcaseApi();
 
 const { token } = userStore();
 
-interface TemTestCase {
+interface TemTestcase {
     checked: boolean;
     deleted: boolean;
     data: Testcase;
 }
 
-const props = defineProps({
-    problemId: {
-        type: Number,
-        required: false,
-    },
-    testCase: {
-        type: Object as () => Testcase,
-        required: false,
-    }
+const props = withDefaults(defineProps<{
+    testcase?: Testcase,
+    problemId?: number,
+}>(),{
+    testcase: () => ({
+        id: 0,
+        serial: 0,
+        problem_id: 0,
+        test_input: '',
+        test_output: '',
+    }),
+    problemId: 0,
 });
 
-const emit = defineEmits(['update:testCase']);
+const emit = defineEmits(['update:testcase']);
+const testcases = ref<TemTestcase[]>([]);
 
-
-const testCases = ref<TemTestCase[]>([]);
-
-const refreshTestCases = async () => {
-    testCases.value = [];
+const refreshTestcases = async () => {
+    testcases.value = [];
     if (props.problemId !== undefined && props.problemId !== 0) {
         await getProblemExecute({
             headers: {
@@ -81,7 +82,7 @@ const refreshTestCases = async () => {
             id: props.problemId,
         }).then((res) => {
             if (res.value?.testcases) {
-                testCases.value = res.value.testcases.map((testcase: Testcase) => {
+                testcases.value = res.value.testcases.map((testcase: Testcase) => {
                     return {
                         checked: false,
                         deleted: false,
@@ -94,25 +95,25 @@ const refreshTestCases = async () => {
 };
 
 const reset = async (id: number) => {
-    const index = testCases.value.findIndex((testcase) => testcase.data.id === id);
+    const index = testcases.value.findIndex((testcase) => testcase.data.id === id);
     if (index !== -1) {
-        await getTestCaseExecute({
+        await getTestcaseExecute({
             headers: {
                 Authorization: `Bearer ${token.value}`,
             },
             id: id,
         }).then((res) => {
             if (res.value) {
-                testCases.value[index].data = res.value;
+                testcases.value[index].data = res.value;
             }
         });
-        testCases.value[index].checked = false;
+        testcases.value[index].checked = false;
     }
 };
 
-const addTestCase = async () => {
-    const maxSerial = Math.max(0, ...testCases.value.map(tc => tc.data.serial));
-    const newTestCase: TemTestCase = {
+const addTestcase = async () => {
+    const maxSerial = Math.max(0, ...testcases.value.map(tc => tc.data.serial));
+    const newTestcase: TemTestcase = {
         checked: true,
         deleted: false,
         data: {
@@ -123,50 +124,69 @@ const addTestCase = async () => {
             test_output: '',
         }
     };
-    testCases.value.push(newTestCase);
+    testcases.value.push(newTestcase);
 };
 
-const handleCurrentChange = async (val: TemTestCase | undefined) => {
+const handleCurrentChange = async (val: TemTestcase) => {
     if (val) {
-        emit('update:testCase', val.data);
+        emit('update:testcase', val.data);
     }
 }
 
-const uploadTestCase = async () => {
+const uploadTestcase = async () => {
     if (props.problemId === 0) {
         ElMessage.error('请先上传题目');
         return;
     }
-    for (const testCase of testCases.value) {
-        if (testCase.deleted) {
-            if (testCase.data.id != undefined && testCase.data.id !== 0) {
-                await deleteTestCaseExecute({
+    for (const testcase of testcases.value) {
+        if (testcase.deleted) {
+            if (testcase.data.id != undefined && testcase.data.id !== 0) {
+                await deleteTestcaseExecute({
                     headers: {
                         Authorization: `Bearer ${token.value}`,
-                    }, id: testCase.data.id
+                    }, id: testcase.data.id
                 });
             }
-        } else if (testCase.checked) {
-            if (testCase.data.id !== 0) {
-                await updateTestCaseExecute({
+        } else if (testcase.checked) {
+            if (testcase.data.id !== 0) {
+                await updateTestcaseExecute({
                     headers: {
                         Authorization: `Bearer ${token.value}`,
-                    }, data: testCase.data
+                    }, data: testcase.data
                 });
             } else {
-                await uploadTestCaseExecute({
+                await uploadTestcaseExecute({
                     headers: {
                         Authorization: `Bearer ${token.value}`,
-                    }, data: testCase.data
+                    }, data: testcase.data
                 });
             }
         }
     };
-    await refreshTestCases();
+    await refreshTestcases();
 };
 
+watch(() => props.testcase, (newTestcase) => {
+    console.log('watch testcase', newTestcase);
+    if (newTestcase) {
+        const index = testcases.value.findIndex(tc => tc.data.id === newTestcase.id);
+        if (index !== -1) {
+            if (JSON.stringify(testcases.value[index].data) !== JSON.stringify(newTestcase)) {
+                testcases.value[index].data = newTestcase;
+                testcases.value[index].checked = true;
+            }
+        } else {
+            testcases.value.push({
+                checked: true,
+                deleted: false,
+                data: newTestcase
+            });
+        }
+    }
+});
+
 defineExpose({
-    refreshTestCases,
+    refreshTestcases,
 })
 </script>
 
