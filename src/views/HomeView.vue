24f9f97318c752f68227cdac4c9e040df0e2b2fb
HEX9CF <hex9cf@aliyun.com>
2024-12-25 10:49:44 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <template>
-  <ElText>我是主页，你是谁？</ElText>
+  <div class="container-main">
+    <el-row>
+      <el-col :span="24">
+        <el-card>
+          <el-carousel height="100" motion-blur>
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -8,3 +20,25 @@ import { onMounted } from 'vue';
 onMounted(() => {
 });
 </script>
+
+<style>
+.demonstration {
+  color: var(--el-text-color-secondary);
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
