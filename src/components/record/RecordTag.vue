71fdf0d7aa8682a986db9c95b63169b4988ff63c
ArtdragonXoX <1262922587@qq.com>
2024-12-26 21:49:51 +0800

    feat(components): 添加记录标签组件
    新增 RecordTag 组件，用于展示记录的状态标签。该组件具有以下功能：
    - 使用 Element UI 的 tooltip 和 tag 组件
    - 根据传入的状态值显示不同的颜色和缩写文本
    - 支持的所有状态定义在 JudgeStatus 枚举中

---
+++ src/components/record/RecordTag.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-tooltip class="box-item" effect="dark" :content="JudgeStatusMap[status]" placement="top">
+        <el-tag :color="JudgeStatusColor[status]" style="color: #fff">
+            {{ JudgeStatusAbbr[status] }}
+        </el-tag>
+    </el-tooltip>
+</template>
+
+<script setup lang="ts">
+import { JudgeStatusMap,JudgeStatusColor,JudgeStatusAbbr, JudgeStatus } from '@/types/Judge';
+
+const props = defineProps<{
+    status: JudgeStatus
+}>();
+</script>
