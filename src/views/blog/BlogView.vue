a04532081cc566d62336231abc7f8bc6473de2e8
HEX9CF <hex9cf@aliyun.com>
2024-12-26 16:54:05 +0800

    Update BlogView.vue

--- src/views/blog/BlogView.vue
+++ src/views/blog/BlogView.vue
@@ -7,6 +7,7 @@ import type { ApiResponse } from "@/types/ApiResponse";
 import { renderMarkAndLaTeX } from "@/utils/renderMarkAndLaTeX";
 import { BlogStatusMap } from "@/types/Blog";
 import {StarFilled} from "@element-plus/icons-vue";
+import {formatDateStr} from "../../utils/date";
 
 const blogId = useRouteParams<number>("id");
 
@@ -36,28 +37,45 @@ onMounted(async () => {
     <br/>
     <el-card>
       <h1>{{ blog?.title }}</h1>
-      <br/>
       <span>
-                  <el-icon><UserFilled/></el-icon>&nbsp;{{ blog?.authorId }}
-                  <el-divider direction="vertical"></el-divider>
-                  <el-icon><Timer/></el-icon>&nbsp;{{ blog?.createTime }}
-                  <el-divider direction="vertical"></el-divider>
-                  <el-icon><Stopwatch /></el-icon>&nbsp;{{ blog?.updateTime }}
-              </span>
+          <el-icon><UserFilled/></el-icon>&nbsp;{{ blog?.user_id }}
+          <el-divider direction="vertical"></el-divider>
+          <el-icon><View /></el-icon>&nbsp;0
+          <el-divider direction="vertical"></el-divider>
+          <el-icon><StarFilled /></el-icon>&nbsp;0
+          <el-divider direction="vertical"></el-divider>
+          <el-icon><Comment /></el-icon>&nbsp;0
+          <el-divider direction="vertical"></el-divider>
+          <el-icon><Share /></el-icon>&nbsp;0
+          <el-divider direction="vertical"></el-divider>
+          <el-icon><Timer/></el-icon>&nbsp;{{ formatDateStr(blog?.create_time) }}
+          <el-divider direction="vertical"></el-divider>
+          <el-icon><Stopwatch /></el-icon>&nbsp;{{ formatDateStr(blog?.update_time) }}
+          <el-divider direction="vertical"></el-divider>
+      </span>
+      <br/><br/>
+      <span>
+        <el-tag>tag</el-tag>&nbsp;
+        <el-tag>tag</el-tag>&nbsp;
+        <el-tag>tag</el-tag>&nbsp;
+        <el-tag>tag</el-tag>&nbsp;
+        <el-tag>tag</el-tag>&nbsp;
+      </span>
       <el-divider></el-divider>
-      {{ blog?.content }}
+      <p>
+        {{ blog?.content }}
+      </p>
       <el-divider></el-divider>
       <span>
-                  <el-button><el-icon><View /></el-icon>&nbsp;0</el-button>
-                  <el-button><el-icon><StarFilled /></el-icon>&nbsp;0</el-button>
-                  <el-button><el-icon><Share /></el-icon>&nbsp;0</el-button>
-                </span>
+        <el-button><el-icon><View /></el-icon>&nbsp;0</el-button>
+        <el-button><el-icon><StarFilled /></el-icon>&nbsp;0</el-button>
+        <el-button><el-icon><Share /></el-icon>&nbsp;0</el-button>
+      </span>
     </el-card>
     <br/>
     <el-card>
       评论区
     </el-card>
-
   </div>
 </template>
 
