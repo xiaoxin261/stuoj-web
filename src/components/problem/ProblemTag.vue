0d17000c256392d849ef24fa746d883d193fa4ea
ArtdragonXoX <1262922587@qq.com>
2024-12-26 21:18:26 +0800

    feat(components): 添加问题标签组件并更新类型声明
    - 新增 ProblemTag 组件，用于展示问题标签
    - 在 components.d.ts 中添加 ProblemTag 组件的类型声明
    - 移除了 components.d.ts 中未使用的 ElAside、ElCheckboxGroup、ElMenu 和 ElMenuItem 组件的声明

---
+++ src/components/problem/ProblemTag.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <el-tag v-for="tag in props.tags" :key="tag.id" :type="tag.color">{{ tag.name }}</el-tag>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { Tag } from '@/types/Problem';
+import { ref, onMounted } from 'vue'
+
+const props = defineProps<{
+    tags: Tag[];
+}>();
+</script>
+
+<style scoped>
+.el-tag {
+    margin-right: 5px;
+}
+</style>
