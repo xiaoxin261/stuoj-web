fb0f51d1d8adaf29dbf194c7278f0aa9e16fbb7f
ArtdragonXoX <1262922587@qq.com>
2024-12-26 22:22:54 +0800

    feat(user): 添加用户角色标签组件
    - 新增 UserRoleTag 组件用于显示用户角色
    - 使用 el-tag 组件展示角色标签，颜色根据角色不同而变化
    - 引入 UserRoleColor 和 UserRoleMap 用于定义角色颜色和名称映射
    - 组件通过 props 接收 role 属性

---
+++ src/views/user/UserRoleTag.vue
@@ -0,0 +1,11 @@
+<template>
+    <el-tag :color="UserRoleColor[role]" style="color: #fff">
+        {{ UserRoleMap[role] }}
+    </el-tag>
+</template>
+
+<script setup lang="ts">
+import { ROLE, UserRoleColor, UserRoleMap } from "@/types/User";
+
+const props = defineProps<{ role: ROLE }>();
+</script>
