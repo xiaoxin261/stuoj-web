fe09c17e331ce78f41bed0e4e65f71e8d85e9b91
ArtdragonXoX <1262922587@qq.com>
2024-12-27 11:59:21 +0800

    feat(components): 新增问题标签组件并优化标签展示和选择功能
    - 新增 ElDescriptions、ElHeader、ElText、ElTooltip 组件类型声明
    - 重构 ProblemTag 组件，支持标签选择和展示分离
    - 新增 ProblemTagSelect 和 ProblemTagShow 组件
    - 优化标签数据同步和更新逻辑
    - 添加垂直和水平布局支持

--- src/components/problem/ProblemTag.vue
+++ src/components/problem/ProblemTag.vue
@@ -1,20 +1,65 @@
 <template>
-    <div>
-        <el-tag v-for="tag in props.tags" :key="tag.id" :type="tag.color">{{ tag.name }}</el-tag>
+    <div :class="['problem-tag', layoutClass]">
+        <ProblemTagSelect v-model:tags="tags" ref="problemTagSelectRef" />
+        <ProblemTagShow class="tag-show" :size="tagsSize" :remove-flag="removeFlag" v-model:tags="tags" />
     </div>
 </template>
 
 <script setup lang="ts">
-import type { Tag } from '@/types/Problem';
-import { ref, onMounted } from 'vue'
+import { ref, computed, watchEffect } from "vue";
+import type { Tag } from "@/types/Problem";
+import ProblemTagSelect from "@/components/problem/ProblemTagSelect.vue";
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
+    removeFlag?: boolean;
     tags: Tag[];
-}>();
+    tagsSize?:'small'|'default'|'large';
+    layout?: 'horizontal' | 'vertical';
+}>(), {
+    removeFlag: false,
+    layout: 'horizontal',
+    tagsSize:'small',
+});
+
+const tags = ref<Tag[]>(props.tags);
+
+const emit = defineEmits(['update:tags']);
+
+const problemTagSelectRef = ref<InstanceType<typeof ProblemTagSelect> | null>(null);
+
+watchEffect(() => {
+    emit('update:tags', tags.value);
+});
+
+watchEffect(() => {
+    tags.value = props.tags;
+});
+const reset = () => {
+    if (problemTagSelectRef.value)
+        problemTagSelectRef.value.resetAndConfirm();
+};
+defineExpose({
+    reset,
+});
+
+const layoutClass = computed(() => props.layout === 'horizontal' ? 'horizontal-layout' : 'vertical-layout');
+
 </script>
 
 <style scoped>
-.el-tag {
-    margin-right: 5px;
+.horizontal-layout {
+    display: flex;
+    flex-direction: row;
+    
+    .tag-show{
+        margin-left: 10px;
+    }
+}
+.vertical-layout {
+    display: flex;
+    flex-direction: column;
+    .tag-show {
+        margin-top: 10px;
+    }
 }
 </style>
