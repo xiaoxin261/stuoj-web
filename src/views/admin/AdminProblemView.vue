aab080347c9d8db267a731ab04caeaad873f4c7c
ArtdragonXoX <1262922587@qq.com>
2024-12-30 01:07:22 +0800

    refactor(admin): 优化问题列表组件
    - 移除了未使用的导入和接口
    - 删除了冗余的样式定义
    - 简化了组件结构，提高了代码可读性

--- src/views/admin/AdminProblemView.vue
+++ src/views/admin/AdminProblemView.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 import { getProblemListApi } from '@/apis/problem';
 import {onMounted, ref} from "vue";
-import { ProblemStatusMap, DifficultyMap, ProblemStatusColor, DifficultyColor } from '@/types/Problem';
-import { formatDateStr } from "@/utils/date";
-import {userStore} from "@/stores/user";
 import type { ProblemInfo } from '@/types/Problem';
 import type { Page } from '@/types/misc';
 import router from '@/router';
 
-interface Scope {
-  row: {
-    difficulty: keyof typeof DifficultyMap;
-    status: keyof typeof ProblemStatusMap;
-  };
-}
 interface ProblemParams {
   page: number
   size: number
@@ -26,7 +17,6 @@ const params = ref<ProblemParams>({
   page: 1,
   size: 10
 });
-const { token } = userStore();
 
 const getList = async () => {
   await execute({
@@ -71,20 +61,4 @@ onMounted (() => {
 </template>
 
 <style scoped>
-a:link {
-  text-decoration: none;
-}
-
-a:visited {
-  text-decoration: none;
-}
-
-a:hover {
-  text-decoration: none;
-}
-
-a:active {
-  text-decoration: none;
-}
-
 </style>
