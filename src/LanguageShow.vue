be0b73d486391179376e02fe8cf664b63e7b306e
ArtdragonXoX <1262922587@qq.com>
2024-12-30 21:45:34 +0800

    feat(components): 添加 LanguageShow 组件
    - 创建 LanguageShow 组件，用于显示编程语言名称
    - 组件通过 langId 属性接收语言 ID，并展示对应的语言名称
    - 如果没有提供 langId 或找不到对应语言，则显示"无"
    - 组件在挂载时获取所有可用语言列表，并根据 props 更新显示的语言

---
+++ src/LanguageShow.vue
@@ -0,0 +1,41 @@
+<template>
+    <ElText v-if="language" size="large" tag="b">{{ language.name }}</ElText>
+    <ElText v-else size="large" tag="b">无</ElText>
+</template>
+
+<script setup lang="ts">
+import { GetLanguages } from '@/apis/judge';
+import type { Language } from '@/types/Judge';
+import { onMounted, ref, watchEffect } from 'vue';
+
+const { execute, state } = GetLanguages();
+const options = ref<Language[]>([]);
+const props = defineProps<{
+    langId?: number;
+}>();
+
+const language = ref<Language | null>(null);
+
+const updateLanguage = () => {
+    if (props.langId !== undefined) {
+        const foundLanguage = options.value.find((item) => item.id === props.langId);
+        language.value = foundLanguage || null;
+    } else {
+        language.value = null;
+    }
+};
+
+onMounted(async () => {
+    await execute();
+    if (state.value) {
+        options.value = state.value;
+        updateLanguage();
+    }
+});
+
+watchEffect(() => {
+    if (options.value && props.langId !== undefined) {
+        updateLanguage();
+    }
+});
+</script>
