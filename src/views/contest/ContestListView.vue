e9cb08944203aec515951140bff8ea9685dfd7cc
HEX9CF <hex9cf@aliyun.com>
2025-01-10 22:34:46 +0800

    Add contest calendar

--- src/views/contest/ContestListView.vue
+++ src/views/contest/ContestListView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import EmptyPage from "@/components/misc/EmptyPage.vue";
+import {ref} from "vue";
+
+const date = ref(new Date());
 </script>
 
 <template>
-  <EmptyPage />
+  <div class="container-main">
+  <el-card style="margin-bottom: 20px">
+    <template #header>
+      <div class="card-header">
+        <strong>比赛日历</strong>
+      </div>
+    </template>
+    <el-calendar v-model="date" />
+  </el-card>
+  </div>
 </template>
 
 <style scoped>
