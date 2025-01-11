aae1e9a90a366f04963dfc88c516abca7e7795ab
ArtdragonXoX <1262922587@qq.com>
2025-01-12 03:07:43 +0800

    feat(components): 新增 ProblemName 和 UserName 组件
    - 添加 ProblemName 组件，用于显示问题名称和难度颜色
    - 添加 UserName 组件，用于显示用户名和角色颜色
    - 实现了根据 ID 获取问题和用户信息的功能
    - 添加了点击跳转到问题和用户详情页面的逻辑

---
+++ src/components/problem/ProblemName.vue
@@ -0,0 +1,55 @@
+<template>
+    <a class="color-none" href="#">
+        <span class="problem-name" :style="{ fontSize: props.size + 'px', color: problemColor }" @click="handleProblemClick">{{ problem_info.title }}</span>
+    </a>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, type PropType } from 'vue';
+import type { Difficulty, ProblemInfo } from '@/types/Problem';
+import { getProblemApi } from '@/apis/problem';
+import router from '@/router';
+import { DifficultyColor, ProblemStatusColor } from '@/types/Problem';
+
+const { execute } = getProblemApi();
+
+const props = defineProps({
+    problem: {
+        type: Object as PropType<ProblemInfo>,
+        default: () => { }
+    },
+    id: {
+        type: Number,
+        default: 0
+    },
+    size:{
+        type: Number,
+        default: 16
+    }
+});
+
+const problem_info = ref<ProblemInfo>(props.problem);
+const problemColor = ref<string>(DifficultyColor[problem_info.value.difficulty]);
+
+onMounted(async () => {
+    if (props.id !== 0) {
+        await execute({ id: props.id }).then((res) => {
+            if (!res.value) {
+                return;
+            };
+            problem_info.value = res.value.problem;
+            problemColor.value = DifficultyColor[problem_info.value.difficulty];
+        });
+    }
+});
+
+const handleProblemClick = () => {
+    router.push(`/problem/${problem_info.value.id}`);
+};
+</script>
+
+<style scoped>
+.problem-name {
+    font-weight: bold;
+}
+</style>
