b4d6a25cd42574bd43ea8f4e443eeb324a8e89f5
ArtdragonXoX <1262922587@qq.com>
2025-01-12 19:15:13 +0800

    refactor(admin): 移除 AdminProblemView 组件中的未使用代码
    - 删除了与问题列表相关的接口导入和未使用的变量
    - 移除了挂载时获取问题列表的逻辑
    - 保留了创建问题的路由跳转函数

--- src/views/admin/AdminProblemView.vue
+++ src/views/admin/AdminProblemView.vue
@@ -1,43 +1,9 @@
 <script setup lang="ts">
-import { getProblemListApi } from '@/apis/problem';
-import {onMounted, ref} from "vue";
-import type { ProblemInfo } from '@/types/Problem';
-import type { Page } from '@/types/misc';
 import router from '@/router';
-
-interface ProblemParams {
-  page: number
-  size: number
-}
-
-const problemPage = ref<Page<"problems", ProblemInfo>>();
-const problems = ref<ProblemInfo[]>([]);
-const { state, execute } = getProblemListApi();
-const params = ref<ProblemParams>({
-  page: 1,
-  size: 10
-});
-
-const getList = async () => {
-  await execute({
-    params: {
-      ...params.value,
-    }
-  });
-
-  if (state.value) {
-    problemPage.value = state.value;
-    problems.value = problemPage.value.problems;
-  }
-}
-
 const handleCreate = () => {
   router.push("/problem/edit");
 }
 
-onMounted (() => {
-  getList();
-})
 </script>
 
 <template>
