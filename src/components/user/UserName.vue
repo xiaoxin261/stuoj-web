71025f6f9de56a631d7a9178155fde5820b15391
ArtdragonXoX <1262922587@qq.com>
2025-01-12 18:36:51 +0800

    perf(user): 优化 UserName 组件的性能和用户体验
    - 在模板中添加 v-if 指令，仅在用户信息存在时渲染链接
    - 使用 onBeforeMount 替代 onMounted，提前加载用户信息
    - 引入 watchEffect 监听 props 变化，实时更新用户信息
    - 优化用户角色颜色的赋值逻辑，提高代码健壮性

--- src/components/user/UserName.vue
+++ src/components/user/UserName.vue
@@ -1,12 +1,12 @@
 <template>
-    <a class="color-none" :href="`/user/${user_info.id}`">
+    <a v-if="user_info && user_info.id" class="color-none" :href="`/user/${user_info.id}`">
         <span class="user-name" :style="{ fontSize: props.size + 'px', color: userColor }">{{ user_info.username
             }}</span>
     </a>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, type PropType } from 'vue';
+import { onBeforeMount, ref, watchEffect, type PropType } from 'vue';
 import type { UserInfo } from '@/types/User';
 import { GetUserInfo } from '@/apis/user';
 import { UserRoleColor } from '@/types/User';
@@ -16,7 +16,7 @@ const { execute } = GetUserInfo();
 const props = defineProps({
     user: {
         type: Object as PropType<UserInfo>,
-        default: () => { }
+        default: () => ({})
     },
     id: {
         type: Number,
@@ -28,19 +28,29 @@ const props = defineProps({
     }
 });
 
-const user_info = ref<UserInfo>(props.user);
-const userColor = ref<string>(UserRoleColor[user_info.value.role]);
+const user_info = ref<UserInfo>({ ...props.user });
+const userColor = ref<string>('');
 
-onMounted(async () => {
+const load = async () => {
     if (props.id !== 0) {
-        await execute({ id: props.id }).then((res) => {
-            if (!res.value) {
-                return;
-            };
-            user_info.value = res.value;
-            userColor.value = UserRoleColor[user_info.value.role];
+        execute({ id: props.id }).then((res) => {
+            if (res.value) {
+                user_info.value = res.value;
+            }
         });
     }
+    if (user_info.value && user_info.value.role) {
+        userColor.value = UserRoleColor[user_info.value.role] || '';
+    }
+};
+
+onBeforeMount(async () => {
+    load();
+});
+
+watchEffect(() => {
+    user_info.value = props.user;
+    load();
 });
 </script>
 
