ca48d8b427676c41b8d03243194a13edd61d21d9
ArtdragonXoX <1262922587@qq.com>
2025-01-13 00:30:21 +0800

    build(dependencies): 添加 sanitize-html 依赖并更新相关配置，避免XSS注入攻击
    - 在 package.json 中添加 sanitize-html 和 @types/sanitize-html 依赖
    - 在 package-lock.json 中更新相关依赖信息
    - 修改 renderMarkAndLaTeX.ts，集成 sanitize-html 进行 HTML 清理

--- src/utils/renderMarkAndLaTeX.ts
+++ src/utils/renderMarkAndLaTeX.ts
@@ -1,6 +1,18 @@
 import { renderMarkDown } from "./renderMarkDown";
 import { renderLaTeX } from "./renderLaTeX";
-
+import sanitizeHtml, { type IOptions } from 'sanitize-html';
 export const renderMarkAndLaTeX = (text: string) => {
-    return renderMarkDown(renderLaTeX(text));
+    const html = renderMarkDown(text);
+    const options: IOptions = {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        allowedAttributes: {
+            ...sanitizeHtml.defaults.allowedAttributes,
+            img: ['src', 'alt', 'width', 'height'],
+            div: ['align']
+        },
+        allowedSchemes: ['http', 'https', 'blob'],
+    };
+    const cleanHtml = sanitizeHtml(html, options);
+    const cleanHtml_ = renderLaTeX(cleanHtml);
+    return cleanHtml_.trim();
 }
