cc500ace8f26cd1b344d20d4ece0124fe600b25f
ArtdragonXoX <1262922587@qq.com>
2025-01-12 14:13:09 +0800

    feat(components): 添加 ScoreShow 组件
    - 创建 ScoreShow 组件，用于展示分数和状态
    - 组件接收 score、size、status、statusShow 和 id 等属性
    - 根据属性值显示分数、状态标签和对应颜色
    - 如果提供 id，组件会自动获取记录信息并更新分数和状态

---
+++ src/components/record/ScoreShow.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <RecordTag v-if="statusShow" :status="status" />
+        <span class="ellipsis score" :style="{ fontSize: props.size + 'px', color: color }">{{ score }}</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, type PropType } from 'vue';
+import { JudgeStatusColor, JudgeStatus } from '@/types/Judge';
+import { getRecordInfoAPI } from '@/apis/record';
+
+const { execute } = getRecordInfoAPI();
+
+const props = defineProps({
+    score: {
+        type: Number,
+        default: 0
+    },
+    size: {
+        type: Number,
+        default: 16
+    },
+    status: {
+        type: Number as PropType<JudgeStatus>,
+        default: JudgeStatus.Unknown
+    },
+    statusShow: {
+        type: Boolean,
+        default: false
+    },
+    id: {
+        type: Number,
+        default: 0
+    }
+});
+
+const score = ref(props.score);
+const status = ref(props.status);
+const color = ref(JudgeStatusColor[status.value]);
+
+onMounted(async () => {
+    if (props.id !== 0) {
+        await execute({ id: props.id }).then((res) => {
+            if (!res.value) {
+                return;
+            };
+            score.value = res.value.submission.score ?? 0;
+            status.value = res.value.submission.status;
+        });
+    };
+    color.value = JudgeStatusColor[status.value];
+});
+
+</script>
+
+<style scoped>
+.scorp {
+    font-weight: bold;
+}
+div {
+    width: 40px;
+    display: flex;
+    flex-direction: column;
+}
+</style>
