34b1d477327d7bed0ca7f0db25ec32280ac7aaf0
ArtdragonXoX <1262922587@qq.com>
2025-01-14 01:15:35 +0800

    refactor(record): 重构记录组件并添加筛选功能
    - 修改 Records.vue 组件，添加 select 属性控制筛选框显示
    - 优化 UserRecord.vue 组件，使用 Records.vue 组件显示用户记录
    - 在 Records.vue 中添加 userId 和 problemId 属性，用于筛选记录
    - 调整布局和逻辑，提高组件的可复用性和灵活性

--- src/components/user/UserRecord.vue
+++ src/components/user/UserRecord.vue
@@ -1,70 +1,14 @@
 <template>
   <ElCard>
-    <RecordList :admin="admin" v-model:submissions="submissions" @delete="handleQuery" />
-    <br />
-    <ElPagination class="pagination" v-model:current-page="paramsPage.page" v-model:page-size="paramsPage.size"
-                  :page-sizes="[10, 20, 50, 100]" :size="'small'" :background="true"
-                  layout="total, sizes, prev, pager, next, jumper" :total="recordPage?.total" @size-change="handleQuery"
-                  @current-change="handleQuery" />
+    <Records :user-id="props.userId" :select="false" />
   </ElCard>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { getRecordListApi } from '@/apis/record';
-import { type RecordParams, type Submission } from '@/types/Record';
-import { type Page } from '@/types/misc';
-import { useRoute } from 'vue-router';
-
-const { execute } = getRecordListApi();
-
 const props = withDefaults(defineProps<{
-  admin?: boolean;
   userId?: number;
 }>(), {
-  admin: false,
   userId: 0
-});
-
-const paramsSelect = ref<RecordParams>({
-  page: 1,
-  size: 20,
-  user: props.userId,
-});
-
-const paramsPage = ref<RecordParams>({
-  page: 1,
-  size: 20,
-});
-
-
-const params = ref<RecordParams>({
-  page: 1,
-  size: 20,
-});
-
-const route = useRoute();
-
-const submissions = ref<Submission[]>([]);
-
-const recordPage = ref<Page<"submissions", Submission>>();
-
-const handleQuery = async () => {
-  params.value = {
-    ...paramsSelect.value,
-    page: paramsPage.value.page,
-    size: paramsPage.value.size,
-  }
-  await execute({
-    params: params.value
-  }).then((res) => {
-    submissions.value = res.value?.submissions || [];
-    recordPage.value = res.value;
-  });
-}
-
-onMounted(() => {
-  handleQuery();
 });
 
 </script>
