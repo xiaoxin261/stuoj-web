e88c59bfc6740ab446826af31bd37fb0eef4f3c4
HEX9CF <hex9cf@aliyun.com>
2025-01-14 00:54:45 +0800

    Update FooterBar.vue

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -5,8 +5,8 @@ const year = new Date().getFullYear();
 <template>
   <div class="footer">
     <div class="about">
-      版权所有 &copy; 2024 - {{year}} STUOJ<br/>
-      本项目采用 GPL-3.0 license 进行许可<br/>
+      Copyright © 2024-{{ year }} STUOJ. All Rights Reserved.<br/>
+      GNU GENERAL PUBLIC LICENSE Version 3<br/>
       <a href="https://github.com/STUOJ">汕头大学疾风算法协会 STUOJ 开发组</a><br/>
     </div>
   </div>
